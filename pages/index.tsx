08e4a4f0021d62e7c8f4e5f26b9972fbd5e33ac6
asurance <784672616@qq.com>
2021-01-18 19:01:00 +0800

    fix(home): 取消首页调试用链接

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const Home = (): JSX.Element => {
     return <Layout >
         <header><h1>Flex布局手册</h1></header>
-        <Link href="/container/FlexDirection">start</Link>
     </Layout>
 }
 
