b81608e39cec9aa2cc8cd6bbc207d96fe7950476
asurance <784672616@qq.com>
2021-01-18 18:15:07 +0800

    feat(404): 自定义404页面

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from 'react'
+import Layout from '../components/Layout'
+
+export default function Custom404(): ReactElement {
+    return <Layout title="404">
+        <header><h1>404</h1></header>
+    </Layout>
+}
