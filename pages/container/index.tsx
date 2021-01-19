2309cb7e7b0ecaf0855c40cf50aa162ab3de5cec
asurance <784672616@qq.com>
2021-01-19 16:03:53 +0800

    perf(container): 优化container显示

--- pages/container/index.tsx
+++ pages/container/index.tsx
@@ -4,8 +4,8 @@ import Layout from '../../components/Layout'
 import PropertyP from '../../components/PropertyP'
 
 export default function Container(): ReactElement {
-    return <Layout>
-        <header><h1>容器(Container)</h1></header>
+    return <Layout title="container">
+        <header><h1>容器(container)</h1></header>
         <PropertyP>{ContainerLink}是一个display设置为flex/inline-flex的元素生成的框。</PropertyP>
     </Layout>
 }
