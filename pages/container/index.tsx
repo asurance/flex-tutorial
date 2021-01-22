336d927c4bc696f5b5ef61146a930fc62c2cb075
asurance <784672616@qq.com>
2021-01-22 18:41:03 +0800

    feat(container): 基本完成

--- pages/container/index.tsx
+++ pages/container/index.tsx
@@ -6,6 +6,8 @@ import PropertyP from '../../components/PropertyP'
 export default function Container(): ReactElement {
     return <Layout title="container">
         <header><h1>容器(container)</h1></header>
-        <PropertyP>{ContainerLink}是一个display设置为flex/inline-flex的元素生成的框。</PropertyP>
+        <section>
+            <PropertyP>{ContainerLink}为其内容建立一个弹性格式上下文。这与块级上下文类似,只是采用弹性布局代替块布局。</PropertyP>
+        </section>
     </Layout>
 }
