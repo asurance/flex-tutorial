5e75e730863f75478132276ff9b4dc30273120cf
asurance <784672616@qq.com>
2021-01-22 13:58:45 +0800

    refactor(align-items): 优化css类名称

--- pages/container/AlignItems.tsx
+++ pages/container/AlignItems.tsx
@@ -27,6 +27,10 @@ const headNavs: NavVO[] = [
     },
 ]
 
+const ContainerClassNames = [FlexStyles['container-larger']]
+const ItemClassNames = [FlexStyles['item-auto-height']]
+const ItemLargerClassNames = [...ItemClassNames, FlexStyles['item-larger']]
+
 export default function AlignItems(): ReactElement {
     return <Layout title="align-items" navs={headNavs}>
         <header><h1>align-items</h1></header>
@@ -36,50 +40,50 @@ export default function AlignItems(): ReactElement {
         <PropertyValue value="stretch" isDefault>
             <PropertyP>如果{Item}的交叉大小和{CrossAxis}的两侧都不是计算为&apos;auto&apos;,那{Item}会被拉伸。它所使用的值会是使{Item}的交叉大小应可能与行的一致,同时又满足的{MinHeight}/{MinWidth}/{MaxHeight}/{MaxWidth}约束。</PropertyP>
             <section>
-                <FlexContainer className={[FlexStyles['align-items-container']]}>
-                    <FlexItem className={[FlexStyles['align-items-item']]}>1</FlexItem>
-                    <FlexItem className={[FlexStyles['align-items-item']]}>2</FlexItem>
-                    <FlexItem className={[FlexStyles['align-items-item'], FlexStyles['align-items-special-item']]}>3</FlexItem>
+                <FlexContainer className={ContainerClassNames}>
+                    <FlexItem className={ItemClassNames}>1</FlexItem>
+                    <FlexItem className={ItemClassNames}>2</FlexItem>
+                    <FlexItem className={ItemLargerClassNames}>3</FlexItem>
                 </FlexContainer>
             </section>
         </PropertyValue>
         <PropertyValue value="flex-start" >
             <PropertyP>{Item}的{CrossStart}和行的{CrossStart}对齐。</PropertyP>
             <section>
-                <FlexContainer style={{ alignItems: 'flex-start' }} className={[FlexStyles['align-items-container']]}>
-                    <FlexItem className={[FlexStyles['align-items-item']]}>1</FlexItem>
-                    <FlexItem className={[FlexStyles['align-items-item']]}>2</FlexItem>
-                    <FlexItem className={[FlexStyles['align-items-item'], FlexStyles['align-items-special-item']]}>3</FlexItem>
+                <FlexContainer style={{ alignItems: 'flex-start' }} className={ContainerClassNames}>
+                    <FlexItem className={ItemClassNames}>1</FlexItem>
+                    <FlexItem className={ItemClassNames}>2</FlexItem>
+                    <FlexItem className={ItemLargerClassNames}>3</FlexItem>
                 </FlexContainer>
             </section>
         </PropertyValue>
         <PropertyValue value="flex-end" >
             <PropertyP>{Item}的{CrossEnd}和行的{CrossEnd}对齐。</PropertyP>
             <section>
-                <FlexContainer style={{ alignItems: 'flex-end' }} className={[FlexStyles['align-items-container']]}>
-                    <FlexItem className={[FlexStyles['align-items-item']]}>1</FlexItem>
-                    <FlexItem className={[FlexStyles['align-items-item']]}>2</FlexItem>
-                    <FlexItem className={[FlexStyles['align-items-item'], FlexStyles['align-items-special-item']]}>3</FlexItem>
+                <FlexContainer style={{ alignItems: 'flex-end' }} className={ContainerClassNames}>
+                    <FlexItem className={ItemClassNames}>1</FlexItem>
+                    <FlexItem className={ItemClassNames}>2</FlexItem>
+                    <FlexItem className={ItemLargerClassNames}>3</FlexItem>
                 </FlexContainer>
             </section>
         </PropertyValue>
         <PropertyValue value="center" >
             <PropertyP>{Item}的边距框位于行内{CrossAxis}的中心。(如果该行的尺寸小于所需尺寸,则会在两边均匀地溢出。)</PropertyP>
             <section>
-                <FlexContainer style={{ alignItems: 'center' }} className={[FlexStyles['align-items-container']]}>
-                    <FlexItem className={[FlexStyles['align-items-item']]}>1</FlexItem>
-                    <FlexItem className={[FlexStyles['align-items-item']]}>2</FlexItem>
-                    <FlexItem className={[FlexStyles['align-items-item'], FlexStyles['align-items-special-item']]}>3</FlexItem>
+                <FlexContainer style={{ alignItems: 'center' }} className={ContainerClassNames}>
+                    <FlexItem className={ItemClassNames}>1</FlexItem>
+                    <FlexItem className={ItemClassNames}>2</FlexItem>
+                    <FlexItem className={ItemLargerClassNames}>3</FlexItem>
                 </FlexContainer>
             </section>
         </PropertyValue>
         <PropertyValue value="baseline" >
             <PropertyP>{Item}会是用基线进行对齐。所有{Item}会被排布成他们的基线对齐,同时{Item}中基线和{CrossStart}距离最大的一个的与行的{CrossStart}对齐。如果一个{Item}在必要的轴线中没有基线,那会从它的边框中合成基线。</PropertyP>
             <section>
-                <FlexContainer style={{ alignItems: 'baseline' }} className={[FlexStyles['align-items-container']]}>
-                    <FlexItem className={[FlexStyles['align-items-item']]}>1</FlexItem>
-                    <FlexItem className={[FlexStyles['align-items-item']]}>2</FlexItem>
-                    <FlexItem className={[FlexStyles['align-items-item'], FlexStyles['align-items-special-item']]}>3</FlexItem>
+                <FlexContainer style={{ alignItems: 'baseline' }} className={ContainerClassNames}>
+                    <FlexItem className={ItemClassNames}>1</FlexItem>
+                    <FlexItem className={ItemClassNames}>2</FlexItem>
+                    <FlexItem className={ItemLargerClassNames}>3</FlexItem>
                 </FlexContainer>
             </section>
         </PropertyValue>
